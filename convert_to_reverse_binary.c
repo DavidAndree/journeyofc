23df364a513d425f2262fe1ca82367272cee60f1
DavidAndree <davidalvaradoc0106@gmail.com>
2025-10-07 22:03:47 -0400

    Add header comment explaining program functionality

--- convert_to_reverse_binary.c
+++ convert_to_reverse_binary.c
@@ -1,3 +1,6 @@
+/*
+ * A program that takes in a positive integer as input, and outputs a string of 1's and 0's representing the integer in reverse binary.
+ */
 #include <stdio.h>
 
 int main(void) {
